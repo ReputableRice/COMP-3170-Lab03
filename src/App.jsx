e0dd67af6bbaeb0a14656ad570d9a1264bb52b5a
Skailodon <jackiehtruong@outlook.com>
2024-09-23 14:30:13 -0700

    Inserted backup image function in card.tsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,20 @@ function App() {
 
   // Get stories from news feed
   const stories = news_feed.results;
-  // const storyContent = stories.map((story) => <div>{story}</div>)
+  const storyContent = stories.map((stories) =>
+    <Card
+      title={stories.title}
+      image={stories.image_url}
+      creator={stories.creator}
+      description={stories.description}
+    />
+  )
 
   return (
     <div className="App">
       <h1>Random News Feed</h1>
       <div className="feed">
-        <Card />
-        <Card />
+        {storyContent}
       </div>
     </div>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,37 @@
-export default function Card() {
+export default function Card(props) {
+
+    const imageSource = props.image ? props.image : "https://placehold.co/600x400?text=News+Story"
+
     return (
+        // <div className="story">
+        //     <span className="delete">x</span>
+        //     <div className="story_header">
+        //         <div className="story_image">
+        //             <img className="" src="" alt="alt text" />
+        //         </div>
+        //         <div>
+        //             <h1>
+        //                 <a href="">Title</a>
+        //             </h1>
+        //             <span className="story_creator">By Name</span>
+        //         </div>
+        //     </div>
+        //     <p className="description">descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription</p>
+        // </div>
         <div className="story">
             <span className="delete">x</span>
             <div className="story_header">
                 <div className="story_image">
-                    <img className="" src="" alt="alt text" />
+                    <img className="" src={imageSource} alt="alt text" />
                 </div>
                 <div>
                     <h1>
-                        <a href="">Title</a>
+                        <a href="">{props.title}</a>
                     </h1>
-                    <span className="story_creator">By Name</span>
+                    <span className="story_creator">By {props.creator}</span>
                 </div>
             </div>
-            <p className="description">descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription</p>
+            <p className="description">{props.description}</p>
         </div>
     )
 }
