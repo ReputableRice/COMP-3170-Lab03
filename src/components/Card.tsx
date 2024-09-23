806ed8a260d28e7391fd703c038eb6bfe0533ad5
Skailodon <jackiehtruong@outlook.com>
2024-09-23 14:30:49 -0700

    rm old card.tsx code

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,21 +3,6 @@ export default function Card(props) {
     const imageSource = props.image ? props.image : "https://placehold.co/600x400?text=News+Story"
 
     return (
-        // <div className="story">
-        //     <span className="delete">x</span>
-        //     <div className="story_header">
-        //         <div className="story_image">
-        //             <img className="" src="" alt="alt text" />
-        //         </div>
-        //         <div>
-        //             <h1>
-        //                 <a href="">Title</a>
-        //             </h1>
-        //             <span className="story_creator">By Name</span>
-        //         </div>
-        //     </div>
-        //     <p className="description">descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription</p>
-        // </div>
         <div className="story">
             <span className="delete">x</span>
             <div className="story_header">
