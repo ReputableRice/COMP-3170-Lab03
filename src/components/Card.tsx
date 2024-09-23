d4c83b392efecbc7c4e2130f1b9320533f70d081
Skailodon <jackiehtruong@outlook.com>
2024-09-23 15:02:22 -0700

    setup usestate for card visibility (card.tsx)

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,22 +1,33 @@
+import { useState } from "react"
+
 export default function Card(props) {
+    const [isVisible, setIsVisble] = useState(true)
 
     const imageSource = props.image ? props.image : "https://placehold.co/600x400?text=News+Story"
 
+    function CardVisibility() {
+        setIsVisble(false)
+    }
+
     return (
-        <div className="story">
-            <span className="delete">x</span>
-            <div className="story_header">
-                <div className="story_image">
-                    <img className="" src={imageSource} alt="alt text" />
+        <>
+            {isVisible ?
+                <div className="story">
+                    <span className="delete" onClick={CardVisibility}>x</span>
+                    <div className="story_header">
+                        <div className="story_image">
+                            <img className="" src={imageSource} alt="alt text" />
+                        </div>
+                        <div>
+                            <h1>
+                                <a href="">{props.title}</a>
+                            </h1>
+                            <span className="story_creator">By {props.creator}</span>
+                        </div>
+                    </div>
+                    <p className="description">{props.description}</p>
                 </div>
-                <div>
-                    <h1>
-                        <a href="">{props.title}</a>
-                    </h1>
-                    <span className="story_creator">By {props.creator}</span>
-                </div>
-            </div>
-            <p className="description">{props.description}</p>
-        </div>
+                : null}
+        </>
     )
 }
